745bfbd307dc53efd2c362dc4637c8c57ef6ca7c
bongekile23 <nkosibl8@gmail.com>
2023-11-18 15:58:21 +0000

    mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
-# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
-# Usage: ./1-filter_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+
+"""Module that lists all states from the hbtn_0e_0_usa database."""
+
 import sys
+
 import MySQLdb
 
+
 if __name__ == "__main__":
+
+    # Get MySQL credentials from command-line arguments
+
+    # Connect to MySQL server
+
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
     c = db.cursor()
+
+
+    # Execute the SQL query to retrieve all states sorted by id
+
     c.execute("SELECT * FROM `states` ORDER BY `id`")
+
     [print(state) for state in c.fetchall() if state[1][0] == "N"]
